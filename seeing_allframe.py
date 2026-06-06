c28d0d0adfacb71b670a14bdd450bd2e756d39b8
Mangostin <Arikui57@outlook.jp>
2026-06-06 13:03:04 +0900

    seeing_allframeを作成しました。 15.32it/sなので、もう少し早くしたいです。

--- seeing_allframe.py
+++ seeing_allframe.py
@@ -1,168 +1,102 @@
-#-- ライブラリのインポート --
-#画像を読み込み、円を検出するライブラリ
 import cv2
-#数学の計算をするライブラリ
 import numpy as np
-#プロットするライブラリ
-import matplotlib.pyplot as plt
-#ファイルのリストを作るライブラリ
-import os
-import shutil
-#csvファイルの読み出しをするライブラリ
-import pandas as pd
+from decimal import Decimal, ROUND_HALF_UP
+from MIN2_ignore_sunspots import MIN2_ignore_sunspots as MIN2_ver1
+def seeing_one_frame(readed_img,cir_stat,limb_wigth=24,allp_num=1360,show=False,debug=False):
+    """
+    reimg:cv2で読み込んだ画像を渡してください
+    cir_stat:MIN2_ignore_sunspotsの返り値を渡してください。
+    limb_wigth: limbの解析幅
+    show:解析を図示するか
+    debug:途中経過を表示するか
+    """
+    realrlst=[]
+    min2rlst=[]
+    x,y=[],[] if show else (None,None)
+    center,r=cir_stat
+    cx,cy=center
+    if allp_num%4!=0:
+        raise("allp_numは4の倍数にしてください")
+    for i in range(int(-(allp_num/8)),int(allp_num/8)):
+        min2r=np.sqrt(r**2-i**2)#円の中心をとおる水平、垂直の線から目標円周上の点までの距離(近似円)
+        min2rlst+=[float(Decimal(str(min2r)).quantize(Decimal('0.1'), rounding=ROUND_HALF_UP))]#小数点第一位で四捨五入
+        min2r=Decimal(str(min2r)).quantize(Decimal('1'), rounding=ROUND_HALF_UP)#整数に四捨五入、roundは銀行丸目なので注意
+        min2r=float(min2r)#decimalはfloatと計算できないのでfloatに変換
+        #L
+        samples=readed_img[int(cy+i),int(cx-min2r-limb_wigth):int(cx-min2r+limb_wigth)]
+        realindx=np.argmax(np.diff(np.gradient(samples)))+0.5#diffで要素が減る分
+        realrlst+=[min2r+realindx-limb_wigth]
+        if show:
+            x+=[cx-min2r-limb_wigth+realindx]
+            y+=[cy+i]
+        #R
+        samples=readed_img[int(cy+i),int(cx+min2r-limb_wigth):int(cx+min2r+limb_wigth)]
+        realindx=np.argmax(np.diff(np.gradient(samples)))+0.5#diffで要素が減る分
+        realrlst+=[min2r+realindx-limb_wigth]
+        if show:
+            x+=[cx+min2r-limb_wigth+realindx]
+            y+=[cy+i]
+        #T
+        samples=readed_img[int(cy-min2r-limb_wigth):int(cy-min2r+limb_wigth),int(cx+i)]
+        realindx=np.argmax(np.diff(np.gradient(samples)))+0.5#diffで要素が減る分
+        realrlst+=[min2r+realindx-limb_wigth]
+        if show:
+            x+=[cx+i]
+            y+=[cy-min2r-limb_wigth+realindx]
+        #B
+        samples=readed_img[int(cy+min2r-limb_wigth):int(cy+min2r+limb_wigth),int(cx+i)]
+        realindx=np.argmax(np.diff(np.gradient(samples)))+0.5#diffで要素が減る分
+        realrlst+=[min2r+realindx-limb_wigth]
+        if show:
+            x+=[cx+i]
+            y+=[cy+min2r-limb_wigth+realindx]
+    print(f"min2r\nmin:{np.min(min2rlst)},max:{np.max(min2rlst)},mean:{np.mean(min2rlst)},std:{np.std(min2rlst)}") if debug else None
+    std=np.std(np.array(realrlst)-np.repeat(np.array(min2rlst),4))
+    return std if not show else (std,x,y)
 
-#並列処理のライブラリ(あいばら追加)
-import concurrent.futures
-#進行度合いをだすやつ(あいばら追加)
-import tqdm
-import datetime
-import time
-from pathlib import Path
-import glob
-import time
-from KANSUU import MIN2
-from MIN2_ignore_sunspots import MIN2_bury_sunspots
-from sengiries import sengiri_2X_justOUTside as sengiri
-from tkinter import filedialog as fd
-from scipy.interpolate import CubicSpline
-from scipy.optimize import minimize_scalar
-from win11toast import toast
-import sys
-#20260206
-argv=sys.argv
-a1 = sys.argv[1]
-a2=int(sys.argv[2])
+def seeing_show(readed_img,x,y,cir):
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    center,r=cir
+    figure, ax = plt.subplots()
+    ax.imshow(readed_img, cmap='magma')
+    ax.scatter(x, y, color='cyan', label='Detected edge points', s=10)
+    circle = patches.Circle(center, r, fill=False, edgecolor='yellow', linewidth=2)
+    ax.add_patch(circle)
+    plt.show()
 
-#実行とデバックにて走らせてください
-pats=a1
-ind=a2
-outdir=r"C:\Users\fujit\OneDrive\デスクトップ\allfram"
-#outdir=r"C:\Users\fujit\OneDrive\デスクトップ\allfram"
-
-gap = 170
-limb_wigth = 48 #単位はpix
-limb_wigth = int(limb_wigth / 2)
-pad = 5
-
-houghC_param1=30
-houghC_param2=100
-houghTF=True
-
-
-start=time.time()
-def folder_read(fpath):
-    folders = [
-        name for name in os.listdir(fpath)
-        if os.path.isdir(os.path.join(fpath, name))
-        ]
-    return folders
-
-def Kansu(Sam_dir,executor=None):
-    foldername = os.path.basename(os.path.dirname(Sam_dir))
-    #print()
-    #print(foldername)
-    if "LT" in foldername :
-        location = "LoofTop"
-    elif "PL" in foldername :
-        location = "PoolSide"
-    else:
-        location = "Unknown"
-    #Bunsan_Alltime = 0
-    #lstはシーイングサイズを格納
-    lst = []
-    #sizeは太陽のサイズ（半径）を格納するリスト
-    files = glob.glob(Sam_dir+"\\"+"*.tiff") + glob.glob(Sam_dir+"*.tif") + glob.glob(Sam_dir+"*.jpg")
-    #print("files",files)
-    with tqdm.tqdm(total=len(files)) as progress:
-        lsts=ex.map(sengiri, files,func=MIN2_bury_sunspots)
-        for data in lsts:
-            progress.update(1)
-            lst.append(data)
-    st_max = 0
-    st_min = 1000000000
-    counter = 0  
-    mxcount = 0
-    mncount = 0
-    #各フレームの標準偏差をstsに格納
-    sts = []
-    for l in lst:
-        if l is None:
-            continue
-        st = np.std(l)
-        sts.append(float(st))
-        if st_max < st:
-            st_max = st
-            mxcount= counter
-        if st_min > st:
-            st_min = st
-            mncount=counter
-        counter += 1
-
-    #print("MAX",st_max,"frame",mxcount)
-    #print("MIN",st_min,"frame",mncount)
-    print("median",np.median(sts))
-    print("average",np.average(sts))
-    #print("size",np.average(sizes))
-    #print("location",location)
-    return sts
-def llen(ist):
-    return range(len((ist)))
-def make_sheet(result,outpath,mode,nc):
-    colmsnumber=0
-    colms=[]
-    data=[]
+if __name__ == "__main__":
+    from tkinter.filedialog import askdirectory as ask
+    import os
+    import pandas as pd
+    from pathlib import Path
+    from tqdm import tqdm
+    from glob import glob
+    debug=False
+    show=[False,""]#見たい画像のパスを入れてください
+    dirname=ask(title="フォルダを選択してください。(選択dirのサブディレクトリ内の画像が処理されます。)")
+    if debug:    
+        from time import time
+        st=time()
+    p = Path(dirname)
+    results={child.name:[] for child in p.iterdir() if child.is_dir()}
+    files = [glob(dirname+"\\"+cdir+"\\*.tiff", recursive=True) for cdir in results.keys()]
+    with tqdm(total=sum([len(file_list) for file_list in files]), desc="Prossesing files") as pbar:
+        for i,filelst in enumerate(files):
+            rere=[]
+            for file in filelst:
+                readed_img=cv2.imread(file, cv2.IMREAD_UNCHANGED)
+                cir_stat=MIN2_ver1(((readed_img >> 8).astype("uint8")),n=10,light_threshold=50,limb_wigth=24,show=show[0],debug=False)
+                if show[0] and file.endswith(show[1]):
+                    std,x,y=seeing_one_frame(readed_img,cir_stat,limb_wigth=24,allp_num=1360,show=True,debug=debug)
+                    seeing_show(readed_img,x,y,cir_stat)
+                else:
+                    std=seeing_one_frame(readed_img,cir_stat,limb_wigth=24,allp_num=1360,show=False,debug=debug)
+                rere.append(std)
+                pbar.update(1)
+            results[list(results.keys())[i]]=rere
+    contents=["mean","min","max","std","median"]
+    Df=pd.DataFrame({cdir:pd.Series(results[cdir]).agg(contents) for cdir in results.keys()})
+    print(Df)
+    print(f"seeing_one_frameの解析時間:{time()-st}秒") if debug else None
     
-    if mode=="a": 
-        df = pd.read_excel(outpath)
-        colms = df.columns.tolist()   # 列名のリスト
-        data = df.values.tolist()       #データのリスト
-    else:
-        colms = []
-        data=[]
-    colmsnumber=len(colms)
-    colms.append(nc)
-    if len(data)>len(result):
-        for i in range(len(data)-len(result)):
-            result.append(None)
-    for re in llen(result):
-        if len(data)<=re:
-            data.append([])
-        for i in range(colmsnumber-len(data[re])):
-            data[re].append(None)
-        data[re].append(result[re])
-        
-    return data,colms   
-if __name__=="__main__":#関数の呼び出し時は起動しない
-    foldist=folder_read(pats)
-    #print("foldist",foldist)
-    if foldist==[]:
-        foldist=[pats]
-    #もしpats内にディレクトリがなければpats内の画像を処理するものと解釈
-    for i in foldist:
-        print("I DO",foldist.index(i),"/in",len(foldist))
-        try:
-            if pats != i:#pats内にディレクトリがない場合に困るから条件分岐
-                patt=pats+"\\"+(i)
-                out_path=outdir+"\\"+pats.split("\\")[ind]+"_by_"+__file__.split("\\")[-1]+".xlsx"#出力xlsxのパスを作成
-            else:
-                patt=pats
-                out_path=outdir+"\\"+pats.split("\\")[-1]+"_by_"+__file__.split("\\")[-1]+".xlsx"#出力xlsxのパスを作成
-            with concurrent.futures.ProcessPoolExecutor(max_workers=os.cpu_count()) as ex:
-                #↑並列処理用のやつ
-                result=Kansu(patt,ex)#主処理、フォルダのパスと並列処理のやつが引数。
-
-            
-            mode="a" if os.path.exists(out_path) else "w"#出力xlsxが既存か否かによってmake_sheetのmodeを変更
-            data,colms=make_sheet(result,out_path,mode,nc=i)#xlsxのデータを作成
-            df = pd.DataFrame(data, columns=colms)#書き込み用のやつ
-            df.to_excel(out_path, index=False)#書き込み
-
-            toast("Python通知", f"{i}が終了しました\nfrom {__file__.split("\\")[-1]}")
-            print(patt)
-            print(out_path)
-        except:
-            error_reason = str(sys.exc_info()[0]) + ": " + str(sys.exc_info()[1])
-            print(error_reason)
-            toast("Python通知",f"ERROR\nwhy error{error_reason}\nfrom {__file__.split("\\")[-1]}")
-    toast("Python通知",f"{pats}の処理がすべて終了しました。\nfrom {__file__.split("\\")[-1]}")
-    print("time:",start-time.time())
