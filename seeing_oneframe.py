64e6f5511d01ebef23fa0a2f92941998b6c4b08a
Mangosutin <mangostin877@gmail.com>
2026-06-12 01:10:55 +0900

    作成したdebugtoolをseeingoneframeに継承。この機会にsengiri.pyを削除

--- seeing_oneframe.py
+++ seeing_oneframe.py
@@ -2,6 +2,71 @@ import cv2
 import numpy as np
 from decimal import Decimal, ROUND_HALF_UP
 from MIN2_ignore_sunspots import MIN2_ignore_sunspots as MIN2_ver1
+class seeing_debug_tools:
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    def where_diff_grad_smale(img:np.ndarray,
+                                min2_edge:tuple,#(x,y)
+                                sample_lib:list, #(startpoint(x,y),endpoint(x,y))
+                                cir_stat:tuple,#(cx,cy),r
+                                gradindx:float|int=None,
+                                diffindx:float|int=None,
+                                error:str="None",
+                                title:str="None"):
+            """主にdebug用です。sampleが画像上のどこなのかを見せてくれます。"""
+            #grad,diffの位置を確認する際に描画の問題なのか実際の場所が違うのかを確認するためにindxを受け取る
+            print("sample lib",sample_lib)
+            print("gradindx(fis)",gradindx)
+            print("diffindx(sed)",diffindx)
+            (cx,cy),r=cir_stat
+            fig, ax = plt.subplots()
+            ax.imshow(img, cmap='magma')
+            sample_lib=[sample_lib[0][0],sample_lib[1][0]],[sample_lib[0][1],sample_lib[1][1]]
+            ax.plot( sample_lib[0],sample_lib[1], color='#09fb7e', label='sample', alpha=0.8, linewidth=2)
+            ax.scatter( min2_edge[0], min2_edge[1], color='cyan', label='MIN edge', s=15)
+            ax.add_patch(patches.Circle((cx, cy), r, fill=False, edgecolor='yellow', linewidth=2))
+            if gradindx is not None:
+                if sample_lib[1][0]==sample_lib[1][1]:#水平
+                    print("horizontal") if debug else None
+                    gradxy=sample_lib[0][0]+gradindx+0.5,sample_lib[1][1]
+                else:#垂直
+                    print("vertical") if debug else None
+                    gradxy=sample_lib[0][0],sample_lib[1][0]+gradindx+0.5
+                ax.scatter( gradxy[0], gradxy[1], color='red', label='Gradient max-first', s=25)
+            if diffindx is not None:
+                if sample_lib[1][1]==sample_lib[1][0]:#水平
+                    print("horizontal") if debug else None
+                    diffxy=sample_lib[0][0]+diffindx+0.5,sample_lib[1][1]
+                else:#垂直
+                    print("vertical") if debug else None
+                    diffxy=sample_lib[0][0],sample_lib[1][0]+diffindx+0.5
+                ax.scatter( diffxy[0], diffxy[1], color='blue', label='Difference max-second', s=25)
+            fig.text(0.99, 0.01, f'error reason: {error}', ha='right', va='bottom', fontsize=15, color='gray')
+            fig.suptitle(title)
+            ax.legend()
+            plt.show(block=True)
+            return None
+    def show_samples(limb_wigth,sample,place,e,gaps):
+                    #縁サンプルの折れ線
+                    fig, ax = plt.subplots()
+                    # タイトル
+                    fig.suptitle(f"{place}_{gaps}")
+                    fig.text(0.5, 0.92, f"error reason: {e}", ha='center')
+                    # 第2軸（右）
+                    ax.plot(sample, color="green", label="sample_ax1",linewidth=0.7,alpha=0.7)
+                    # 第1軸（左）
+                    ax2 = ax.twinx()
+                    ax2.plot(np.gradient(sample), label="grad(sample)_ax2",linewidth=0.7)
+                    ax2.plot(np.diff(np.gradient(sample)), label="diff(grad(sample))_ax2",linewidth=0.7)
+                    ax2.scatter(np.argmax(np.gradient(sample)), np.gradient(sample)[np.argmax(np.gradient(sample))], color="red", label="Gradient max(ax2)")
+                    ax2.scatter(np.argmax(np.diff(np.gradient(sample))), np.diff(np.gradient(sample))[np.argmax(np.diff(np.gradient(sample)))], color="blue", label="Difference max(ax2)")
+                    #min2の縁をプロット
+                    ax2.axvline(x=limb_wigth, color="gray", label="min2 edge", linestyle="--")
+                    # 凡例をまとめる
+                    lines1, labels1 = ax.get_legend_handles_labels()
+                    lines2, labels2 = ax2.get_legend_handles_labels()
+                    ax.legend(lines1 + lines2, labels1 + labels2, loc="upper right")
+                    plt.show(block=True)
 def seeing_one_frame(readed_img,cir_stat,limb_wigth=24,allp_num=1360,show=False,debug=False):
     """
     reimg:cv2で読み込んだ画像を渡してください
